fbd58aca6f23ffd87e2a621c5080fa2fa3daaf02
Agbe Terseer <agbe.terseer@gmail.com>
2020-02-10 08:58:26 +0100

    first commit

---
+++ cred.js
@@ -0,0 +1,9 @@
+module.exports = {
+    AT:{
+        apiKey: "3c06560bfbd89c83eb1baebd554037641ade8ae5e75da8aa74c62383007c1158",
+        username: "sandbox",
+        format: 'json'
+    },
+}
+
+
